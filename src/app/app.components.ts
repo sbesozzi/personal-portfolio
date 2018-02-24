e6481af88f088412eb7647760a87d9ead8ec484b
sbesozzi <sarah.besozzi@gmail.com>
2018-02-24 16:27:34 -0500

    added contact component and logo for home button

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -1,6 +1,7 @@
 import { AboutComponent } from './components/about/about.component';
 import { AppComponent } from './components/app/app.component';
 import { BodyComponent } from './components/body/body.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { WorkComponent } from './components/work/work.component';
 
@@ -8,6 +9,7 @@ export const APP_COMPONENTS = [
     AboutComponent,
     AppComponent,
     BodyComponent,
+    ContactComponent,
     HomeComponent,
     WorkComponent
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { WorkComponent } from './components/work/work.component';
-
 
 export const APP_ROUTES = [
   {
@@ -13,5 +13,6 @@ export const APP_ROUTES = [
   },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'work', component: WorkComponent }
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent }
 ];
