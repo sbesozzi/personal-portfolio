9e01c3aa8d5974cc5912207101513470f17ccb30
sbesozzi <sarah.besozzi@gmail.com>
2018-02-24 13:38:07 -0500

    moved footer to app and info to about view

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'app';
+
+    public goToTumblr() {
+        window.open('http://daydreamsonvinyl.tumblr.com', '_blank');
+    }
+
 }
+
